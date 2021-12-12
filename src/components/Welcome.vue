7735a5703a4e89a6dbaf589c1692feedfb54ec2f
martisaw <martin.sawilla@gmail.com>
2021-12-12 23:27:10 +0100

    fix: images broken in build

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -8,14 +8,14 @@ defineProps({
 </script>
 
 <template>
-<a :href="href" class="group">
+<router-link to="/" class="group">
     <div class="relative">
         <img class="" :src="img">
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div class="bg-gray-900 bg-opacity-50 rounded-md px-2 text-neutral-300 font-bold text-3xl">{{text}}</div>
         </div>
     </div>
-</a>
+</router-link>
 <div class="py-1 bg-gray-900"></div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Welcome from '../components/Welcome.vue'
+import strawCup from '../assets/DSC05029.jpg'
+import straw from '../assets/DSC05025.jpg'
+import cup from '../assets/DSC05041.jpg'
+import toothbrush from '../assets/DSC05037.jpg'
+import mask from '../assets/DSC05033.jpg'
 </script>
 
 <template>
-  <Welcome href="" img="./src/assets/DSC05029.jpg" text="The Manifest"></Welcome>
-  <Welcome href="" img="./src/assets/DSC05025.jpg" text="How it works"></Welcome>
-  <Welcome href="" img="./src/assets/DSC05041.jpg" text="Trashwall"></Welcome>
-  <Welcome href="" img="./src/assets/DSC05037.jpg" text="Contribute"></Welcome>
-  <Welcome href="" img="./src/assets/DSC05033.jpg" text="About us"></Welcome>
+  <Welcome href="" :img=strawCup text="The Manifest"></Welcome>
+  <Welcome href="" :img=straw text="How it works"></Welcome>
+  <Welcome href="" :img=cup text="Trashwall"></Welcome>
+  <Welcome href="" :img=toothbrush text="Contribute"></Welcome>
+  <Welcome href="" :img=mask text="About us"></Welcome>
 </template>
