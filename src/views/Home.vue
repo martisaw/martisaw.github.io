15f4007bb62c13cb8f3afdb6ed31ed65e9080b1f
martisaw <martin.sawilla@gmail.com>
2021-12-14 23:04:23 +0100

    feat: coming soon hint on home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Welcome from '../components/Welcome.vue'
+import WelcomeDisabled from '../components/WelcomeDisabled.vue'
 import strawCup from '../assets/DSC05029_mobile.jpg'
 import straw from '../assets/DSC05025_mobile.jpg'
 import cup from '../assets/DSC05041_mobile.jpg'
@@ -8,9 +9,9 @@ import mask from '../assets/DSC05033_mobile.jpg'
 </script>
 
 <template>
-  <Welcome href="" :img=strawCup text="The Manifest"></Welcome>
-  <Welcome href="" :img=straw text="How it works"></Welcome>
-  <Welcome href="" :img=cup text="Trashwall"></Welcome>
-  <Welcome href="" :img=toothbrush text="Contribute"></Welcome>
-  <Welcome href="" :img=mask text="About us"></Welcome>
+  <WelcomeDisabled href="" :img=strawCup text="The Manifest"></WelcomeDisabled>
+  <WelcomeDisabled href="" :img=straw text="How it works"></WelcomeDisabled>
+  <WelcomeDisabled href="" :img=cup text="Trashwall"></WelcomeDisabled>
+  <WelcomeDisabled href="" :img=toothbrush text="Contribute"></WelcomeDisabled>
+  <WelcomeDisabled href="" :img=mask text="About us"></WelcomeDisabled>
 </template>
