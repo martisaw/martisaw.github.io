3011c7eae8633893b331e9b5d1586b939a52bc50
martisaw <martin.sawilla@gmail.com>
2021-12-14 22:08:33 +0100

    feat: bigger navbar button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -25,7 +25,7 @@ function closeMenu(){
 </script>
 
 <template>
-<div class="w-full h-12 bg-gray-900"><!--Spacer for fixed bottom--></div>
+<div class="w-full h-[56px] bg-gray-900"><!--Spacer for fixed bottom--></div>
 <div class="fixed bottom-0 w-full bg-gray-900 bg-opacity-90 text-white rounded-t-2xl">
   <div id="nav" class="grid justify-items-center space-y-2 max-h-0 overflow-hidden transition-[max-height] ease-in">
     <div class="invisible">Spacer</div>
@@ -38,11 +38,11 @@ function closeMenu(){
     <div>Insta Icon</div>
     <div class="invisible">Spacer</div>
   </div>  
-  <div id="menu-close" @click="closeMenu" class="flex justify-center p-2 hidden">
-      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+  <div id="menu-close" @click="closeMenu" class="flex justify-center p-3 hidden">
+      <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
     </div>
-  <div id="menu-icon" @click="openMenu" class="flex justify-center p-2">
-    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16"></path></svg>
+  <div id="menu-icon" @click="openMenu" class="flex justify-center p-3">
+    <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16"></path></svg>
   </div>
 </div>
 </template>
